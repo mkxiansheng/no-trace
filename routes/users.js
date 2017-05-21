cc8bb4fafc5508a7f22197d9c80027f219ce0002
mkxiansheng <huangzhiyong200903@163.com>
2017-05-21 23:42:56 +0800

    update

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/:name', function(req, res) {
-  res.send('hello, ' + req.params.name);
-});
-
-module.exports = router;
